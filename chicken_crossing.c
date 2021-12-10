35cc3f21aaa7dc2947d1f9ef5d36e8a8758315e7
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-12-10 19:29:35 -0300

    Bump version to 0.2

--- chicken_crossing.c
+++ chicken_crossing.c
@@ -582,6 +582,6 @@ void main() {
 }
 
 SMS_EMBED_SEGA_ROM_HEADER(9999,0); // code 9999 hopefully free, here this means 'homebrew'
-SMS_EMBED_SDSC_HEADER(0,1, 2021,12,8, "Haroldo-OK\\2021", "Chicken Crossing",
+SMS_EMBED_SDSC_HEADER(0,2, 2021,12,10, "Haroldo-OK\\2021", "Chicken Crossing",
   "Made for The Honest Jam III - https://itch.io/jam/honest-jam-3.\n"
   "Built using devkitSMS & SMSlib - https://github.com/sverx/devkitSMS");
